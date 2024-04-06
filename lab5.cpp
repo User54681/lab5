cc929829c42d921f83ee0f0a68a92e144a4c7314
User54681 <anzolotylh@gmail.com>
2024-04-06 20:59:56 +0300

    Добавьте файлы проекта.

---
+++ lab5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::string word, line;
+    unsigned cnt {};
+    std::cout << "Введите слово, кол-во которого вы хотите посчитать: " << "\n";
+    std::cin >> word;
+    std::ifstream file("file.txt");
+
+    if (file.is_open()) {
+        std::getline(file, line);
+        for (unsigned i {}; i <= line.length() - word.length();) {
+            size_t position = line.find(word, i);
+            if (position == std::string::npos) break;
+            ++cnt;
+            //i = position + i;
+        }
+    }
+
+    std::cout << cnt << "\n";
+    file.close();
+}
