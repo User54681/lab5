4ea1060e60ab83053a22d5cb7654cad2b2255b12
User54681 <anzolotylh@gmail.com>
2024-04-16 12:22:02 +0300

    переделала меню, добавила функции просмотра, удаления и поиска содержимого файла

--- lab5.cpp
+++ lab5.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
 #include "Source.h"
+#include <algorithm>
 #include <fstream>
 #include <string>
+
+void Edit_Func(unsigned choice);
+void Read_Func(unsigned choice);
 
 int main()
 {
@@ -32,7 +36,62 @@ int main()
 
     if (f.is_open()) {
         f << mat;
+        std::cout << "Запись матрицы в файл завершена" << "\n";
     }
 
     f.close();
+
+    unsigned choice;
+    while (choice != 6) {
+        std::cout << "Выберите для дальнейшего действия: " << "\n";
+        std::cout << "1 - Добавить новую запись в файл" << "\n";
+        std::cout << "2 - Просмотр всех существующих записей в файле" << "\n";
+        std::cout << "3 - Удаление записи из файла" << "\n";
+        std::cout << "4 - Поиск записи в файле" << "\n";
+        std::cout << "5 - Редактирование записи в файле" << "\n";
+        std::cout << "6 - Выход из меню" << "\n";
+        std::cin >> choice;
+        
+        if (choice == 1 || choice == 3 || choice == 5) {
+            Edit_Func(choice);
+        }
+    }
+}
+
+void Edit_Func(unsigned choice) {
+    std::ofstream ff("file3.txt", std::ios_base::app);
+    std::string line, id_in, id;
+
+    if (ff.is_open()){
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Введите строку для записи в файл" << "\n";
+            std::cin >> line;
+            std::replace(line.begin(), line.end(), ' ', '\t');
+            ff << line << "\n";
+            break;
+        case 3:
+            std::cout << "" << "\n";
+            std::cin >> id;
+            
+        default:
+            break;
+        }
+    }
+    ff.close();
+}
+
+void Read_Func(unsigned choice) {
+    std::ifstream fff("file3.txt");
+
+    switch (choice)
+    {
+    case 2:
+
+    case 4:
+
+    default:
+        break;
+    }
 }
